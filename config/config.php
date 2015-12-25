da1ed40340dcb17e287d427c323cdb1f50d15ea4
vish94 <visheshahuja@gmail.com>
2015-12-25 06:59:16 +0000

    Updated config.php

--- config/config.php
+++ config/config.php
@@ -6,10 +6,10 @@
 		defined('DB_PASS') ? null : define('DB_PASS', '');
 		defined('DB_NAME') ? null : define('DB_NAME', 'tinyurl');
 	} else {
-		defined('DB_SERVER') ? null : define('DB_SERVER', 'localhost');
-		defined('DB_USER') ? null : define('DB_USER', 'vish94');
-		defined('DB_PASS') ? null : define('DB_PASS', '');
-		defined('DB_NAME') ? null : define('DB_NAME', 'tinyurl');
+		defined('DB_SERVER') ? null : define('DB_SERVER', 'yourdbserver');
+		defined('DB_USER') ? null : define('DB_USER', 'yourusername');
+		defined('DB_PASS') ? null : define('DB_PASS', 'yourpassword');
+		defined('DB_NAME') ? null : define('DB_NAME', 'yourdbname');
 	}
 
 ?>
